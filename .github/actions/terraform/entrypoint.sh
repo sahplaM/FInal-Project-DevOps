41302e2ada04cfcd82ee4d2ca61b87ea322b73f8
Jonathan Z <111912999+sahplaM@users.noreply.github.com>
2026-04-26 13:08:11 -0500

    Update entrypoint.sh

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -11,7 +11,7 @@ export TF_STAGE=${INPUT_TF_STAGE}
 export DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}
 
 if [[ "$TF_STAGE" == "stage1" ]]; then
-  terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${$INPUT_STATE_KEY}.tfstate"
+  terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
   terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
   terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
 elif [[ "$TF_STAGE" == "stage2" ]]; then
